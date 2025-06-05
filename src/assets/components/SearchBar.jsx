771964ff1856f16fef36d0d4c990ec4acc69dd0e
davideerusso <rd.96.rd@gmail.com>
2025-06-05 18:58:37 +0200

    array unico

--- src/assets/components/SearchBar.jsx
+++ src/assets/components/SearchBar.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 export default function SearchBar() {
   const [searchfilms, setSearchFilms] = useState([]);
   const [films, setFilms] = useState([]);
+  const [serietv, setSerietv] = useState([]);
+  const [all, setAll] = useState([]);
+
   const apiUrl = "https://api.themoviedb.org/3";
   //   const apiKey = "c91cda683436ff534f3d600980a32b8a";
   const query = searchfilms;
@@ -23,10 +26,37 @@ export default function SearchBar() {
         `${apiUrl}/search/movie?api_key=c91cda683436ff534f3d600980a32b8a&language=it_IT&query=${searchfilms}`
       )
       .then((res) => {
-        // const filmSearch = res.data.results;
-        setFilms(res.data.results);
-        console.log(res.data.results);
+        const filmSearch = res.data.results;
+        const newFilms = filmSearch.map((film) => ({
+          type: "Film",
+
+          title: film.title,
+          original_title: film.original_title,
+          original_language: film.original_language,
+          vote_average: film.vote_average,
+        }));
+        setFilms(newFilms);
       });
+
+    axios
+      .get(
+        `${apiUrl}/search/tv?api_key=c91cda683436ff534f3d600980a32b8a&language=it_IT&query=${searchfilms}`
+      )
+      .then((res) => {
+        const SerieSearch = res.data.results;
+
+        const newSeries = SerieSearch.map((serie) => ({
+          type: "serieTv",
+          title: serie.name,
+          original_title: serie.original_name,
+          original_language: serie.original_language,
+          vote_average: serie.vote_average,
+        }));
+        setSerietv(newSeries);
+      });
+    const allItems = [...films, ...serietv];
+    setAll(allItems);
+    console.log("all", allItems);
   }, [searchfilms]);
 
   return (
@@ -42,12 +72,13 @@ export default function SearchBar() {
         ></input>
         <button>Cerca</button>
       </form>
-      {films.map((film) => (
+      {all.map((film) => (
         <div>
           <h2>{film.title}</h2>
           <p>{film.original_title}</p>
           <p>{film.original_language}</p>
           <p>{film.vote_average}</p>
+          <p>{film.type}</p>
         </div>
       ))}
     </>
