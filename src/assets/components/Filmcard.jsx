eb15f14b5f406902770e64ae7a9b0351c63e6cbc
davideerusso <rd.96.rd@gmail.com>
2025-06-06 20:59:38 +0200

    css

--- src/assets/components/Filmcard.jsx
+++ src/assets/components/Filmcard.jsx
@@ -12,30 +12,37 @@ export default function FilmCard({ films, serietv }) {
   };
   return (
     <>
-      <div>
-        <h4>Films</h4>
+      <div className="container container-flex">
         {films.map((item) => (
-          <div key={item.id}>
-            <img src={item.image} />
+          <div key={item.id} className="card-container">
+            <div className="card">
+              <img className="hover-card image-absolute" src={item.image} />
 
-            <h2>{item.title}</h2>
-            <p>{item.original_title}</p>
-            <img src={item.original_language} />
-            <div>{renderStars(item.vote_average)}</div>
-            <p>{item.type}</p>
+              <div className="overlay">
+                <h2>{item.title}</h2>
+                <p>{item.original_title}</p>
+                <img src={item.original_language} />
+                <div>{renderStars(item.vote_average)}</div>
+                <p>{item.type}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
-      <div>
-        <h4>Serie Tv</h4>
+      <div className="container container-flex">
         {serietv.map((itemtv) => (
-          <div key={itemtv.id}>
-            <img src={itemtv.image} />
-            <h2>{itemtv.title}</h2>
-            <p>{itemtv.original_title}</p>
-            <img src={itemtv.original_language} />
-            <div>{renderStars(itemtv.vote_average)}</div>
-            <p>{itemtv.type}</p>
+          <div key={itemtv.id} className="card-container">
+            <div className="card">
+              <img className="hover-card image-absolute" src={itemtv.image} />
+
+              <div className="overlay">
+                <h2>{itemtv.title}</h2>
+                <p>{itemtv.original_title}</p>
+                <img src={itemtv.original_language} />
+                <div>{renderStars(itemtv.vote_average)}</div>
+                <p>{itemtv.type}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
--- src/assets/components/SearchBar.jsx
+++ src/assets/components/SearchBar.jsx
@@ -22,58 +22,22 @@ export default function SearchBar({ allItems }) {
   const handleInputChange = (e) => {
     setSearchFilms(e.target.value);
   };
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       `${apiUrl}/search/movie?api_key=c91cda683436ff534f3d600980a32b8a&query=${searchfilms}&language=it-IT`
-  //     )
-  //     .then((res) => {
-  //       const filmSearch = res.data.results;
-  //       const newFilms = filmSearch.map((film) => ({
-  //         type: "Film",
-  //         id: film.id,
-  //         title: film.title,
-  //         original_title: film.original_title,
-  //         original_language: film.original_language,
-  //         vote_average: Math.floor(film.vote_average),
-  //       }));
-  //       setFilms(newFilms);
-  //     });
-
-  //   axios
-  //     .get(
-  //       `${apiUrl}/search/tv?api_key=c91cda683436ff534f3d600980a32b8a&query=${searchfilms}&language=it-IT`
-  //     )
-  //     .then((res) => {
-  //       const SerieSearch = res.data.results;
-
-  //       const newSeries = SerieSearch.map((serie) => ({
-  //         type: "serieTv",
-  //         id: serie.id,
-  //         title: serie.name,
-  //         original_title: serie.original_name,
-  //         original_language: serie.original_language,
-  //         vote_average: Math.floor(serie.vote_average),
-  //       }));
-  //       setSerietv(newSeries);
-  //     });
-  //   const allItems = [...films, ...serietv];
-  //   setAll(allItems);
-  //   console.log("all", allItems);
-  // }, [searchfilms]);
 
   return (
     <>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          className="input "
-          value={searchfilms}
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Cerca il film"
-        ></input>
-        <button>Cerca</button>
-      </form>
+      <div className="navabar">
+        <img src="../img/Netflix.png" />
+        <form onSubmit={handleFormSubmit}>
+          <input
+            className="input "
+            value={searchfilms}
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Cerca il film"
+          ></input>
+          <button className="button">Cerca</button>
+        </form>
+      </div>
     </>
   );
 }
